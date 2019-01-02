f79bf14f8e09a0b3be6105acc7ee7f55a67f033e
savitskyi-hub <admin@savitskyi.com.ua>
2019-01-02 17:53:57 +0200

    Правки в функционале

--- HelpersJs/namespace.js
+++ HelpersJs/namespace.js
@@ -2,3 +2,34 @@
  * Создаем свою область видимости локальных решений в объекте "BX"
  */
 BX.namespace("SavitskyiHub.BxHelpers.Helpers");
+BX.namespace('SavitskyiHub.BxHelpers.Helpers.Option');
+
+(function() {
+	'use strict';
+
+	/**
+	 *
+	 * @type {{LANGUAGE_ID: string, SITE_DIR: string, SITE_ID: string, init: SavitskyiHub.BxHelpers.Helpers.Option.init}}
+	 */
+	SavitskyiHub.BxHelpers.Helpers.Option = {
+		/**
+		 *
+		 */
+		LANGUAGE_ID : "",
+		SITE_DIR: "",
+		SITE_ID: "",
+		COOKIE_PREFIX: "",
+		SITE_TEMPLATE_PATH: "",
+
+		/**
+		 *
+		 */
+		init : function() {
+			this.LANGUAGE_ID = BX.phpVars.LANGUAGE_ID;
+			this.SITE_DIR = '';
+			this.SITE_ID = '';
+			this.COOKIE_PREFIX = "";
+			this.SITE_TEMPLATE_PATH = "";
+		}
+	};
+})();
